c75c9cd85bc402527a96813f34c817e5c6d592f9
FelipeDireito <fedireito92@gmail.com>
2023-12-04 09:28:04 -0300

    feat: criar receita com fotos e lista de ingredientes

--- backend/schemas/models.py
+++ backend/schemas/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from fastapi import UploadFile, File
 
 class StandartOutput(BaseModel):
     message: str
--- backend/views/recipes.py
+++ backend/views/recipes.py
@@ -1,62 +1,65 @@
-from fastapi import  APIRouter, Request, UploadFile, File, HTTPException, Query
-from schemas.models import ReceitaBasica, BuscaTitulo
+from fastapi import  APIRouter, Request, UploadFile, File, HTTPException, Query, Form
+from fastapi.responses import JSONResponse
+from schemas.models import ReceitaBasica, Ingredientes
 from database.connection import async_session
 from database.models import Receitas, Fotos, Ingrediente
-from sqlalchemy import select
+from sqlalchemy import select, join
 import os
 from typing import List
+import json
 
 recipes = APIRouter()
 
-
-@recipes.post("/receitas")
-async def criar_receita( recipe:ReceitaBasica, request : Request ):
-
-    ip = request.client.host
-
-
-    async with async_session() as sessao:
-        nova_receita = Receitas(
-            titulo=recipe.titulo,
-            instrucoes=recipe.instrucoes
-        )
-        sessao.add(nova_receita)
-        for ingrediente in recipe.ingredientes:
-                    sessao.add(
-                        Ingrediente(
-                            ingrediente=ingrediente.ingrediente,
-                            receitas=nova_receita
-                        )
-                    )
-                    
-        await sessao.commit()
-    return { "message" : "receita adicionada com sucesso!"}
-
-@recipes.post("/foto")
-async def postar_foto( file: UploadFile = File(...) ):
-
+ 
+@recipes.post("/criarReceitas")
+async def criar_receita( 
+    titulo: str = Form(...),
+    instrucoes: str = Form(...),
+    ingredientes: List[str] = Form(...),
+    files: List[UploadFile] = File(...)
+):
     async with async_session() as sessao:
         try:
-            os.makedirs("uploads", exist_ok=True)
-            file_path = os.path.join("uploads", file.filename)
-            with open(file_path, "wb") as image_file:
-                image_file.write(file.file.read())
+            nova_receita = Receitas(
+                titulo=titulo,
+                instrucoes=instrucoes
+            )
+          
+            sessao.add(nova_receita)
 
-            # Save the file path to the database
-            new_photo = Fotos(
-                path=file_path
+            for ingrediente in ingredientes:
+                novo_ingrediente = Ingrediente(
+                    ingrediente=ingrediente,
+                    receita=nova_receita
                 )
+                sessao.add(novo_ingrediente)
 
-            sessao.add(new_photo)
+            os.makedirs("uploads", exist_ok=True)
+
+            file_paths = []
+
+            for file in files:
+                file_path = os.path.join("uploads", file.filename)
+                with open(file_path, "wb") as image_file:
+                    image_file.write(file.file.read())
+                file_paths.append(file_path)
+
+            for file_path in file_paths:
+                nova_foto = Fotos(
+                    foto=file_path,
+                    receita=nova_receita
+                    )
+                sessao.add(nova_foto)
+
             await sessao.commit()
-
-            return {"message": "File uploaded successfully"}
+            return { "message" : "receita adicionada com sucesso!"}
         except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+            return{"error": str(e)}
 
+        
 
 @recipes.post("/fotos")
-async def postar_fotos(id:int, files: List[UploadFile] = File(...)):
+async def postar_fotos(id_receita:int, files: List[UploadFile] = File(...)):
 
     async with async_session() as sessao:
         try:
@@ -75,7 +78,7 @@ async def postar_fotos(id:int, files: List[UploadFile] = File(...)):
             for file_path in file_paths:
                 new_photo = Fotos(
                     foto=file_path,
-                    id_receita=id
+                    id_receita=id_receita
                     )
                 sessao.add(new_photo)
 
@@ -89,11 +92,9 @@ async def postar_fotos(id:int, files: List[UploadFile] = File(...)):
 @recipes.get("/getfotos")
 async def read_photos():
     async with async_session() as session:
-        # Assuming Fotos is your SQLAlchemy model
         result = await session.execute(select(Fotos))
         photos = result.scalars().all()
 
-    # Now 'photos' contains the queried data
     return photos
 
 
@@ -115,3 +116,34 @@ async def busca_titulo(titulo: str = Query(..., title="Title to search")):
         busca = result.scalars().all()
 
     return busca
+
+
+
+@recipes.get("/obter_fotos_por_receita")
+async def obter_fotos_por_receita(receita_id: int):
+    async with async_session() as sessao:
+        try:
+            stmt = (
+                select(Fotos)
+                .select_from(join(Receitas, Fotos, Receitas.id == Fotos.id_receita))
+                .where(Receitas.id == receita_id)
+            )
+            result = await sessao.execute(stmt)
+            fotos = result.scalars().all()
+            return fotos
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+
+@recipes.get("/listar_ingredientes")
+async def listar_ingredientes(receita_id: int):
+    async with async_session() as sessao:
+        stmt = (
+            select(Ingrediente)
+            .join(Receitas)
+            .filter(Receitas.id == receita_id)
+        )
+        result = await sessao.execute(stmt)
+        ingredientes = result.scalars().all()
+
+        return ingredientes
