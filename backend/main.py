4381eaeec59f3234641265b148caffb479eddda5
lucaslobao-18 <lucaslobao14df@gmail.com>
2023-12-08 17:41:20 -0300

    Adiciona a autenticação

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
 from views.post_recipes import recipes
+from views.auth_user import get_users, post_user
 from database.start import create_database
 from views.post_recipes import popula_bd
 from views.get_recipes import get_recipes
+from typing import Annotated
 
 
 
@@ -35,6 +38,9 @@ def inicio():
 
 app.include_router(recipes)
 app.include_router(get_recipes)
+app.include_router(get_users)
+app.include_router(post_user)
+
 
 if __name__ == "__main__":
     import os
@@ -43,3 +49,10 @@ if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True, workers=1)
 
 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+#exemplo de função que utiliza a autenticação como requisito
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
