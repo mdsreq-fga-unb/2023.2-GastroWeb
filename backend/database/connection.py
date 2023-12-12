b716d8e79520e8c72da93a6251ad26405e4cf59a
PedroSampaioDias <pedro.sampaio.dias.rocha@gmail.com>
2023-12-11 23:14:52 -0300

    teste

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,10 +1,10 @@
 from decouple import config
-from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import AsyncSession
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://gastroweb:EjJ789coMnsE0mETtI7Py3uFWTfmHFpo@dpg-clrqfkfqd2ns73ds4n10-a.oregon-postgres.render.com/gastroweb"
+DATABASE_URL = "postgresql+asyncpg://gastroweb:EjJ789coMnsE0mETtI7Py3uFWTfmHFpo@dpg-clrqfkfqd2ns73ds4n10-a.oregon-postgres.render.com/gastroweb"
 
-engine = create_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession)
