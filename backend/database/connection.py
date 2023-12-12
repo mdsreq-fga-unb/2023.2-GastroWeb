df3a9fa14526ddd8dc05fffe1995743d199fd767
PedroSampaioDias <pedro.sampaio.dias.rocha@gmail.com>
2023-12-11 22:34:46 -0300

    spike para deploy

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = config("DATABASE_URL")
+DATABASE_URL = "postgresql://gastroweb:EjJ789coMnsE0mETtI7Py3uFWTfmHFpo@dpg-clrqfkfqd2ns73ds4n10-a.oregon-postgres.render.com/gastroweb"
 
 engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession)
